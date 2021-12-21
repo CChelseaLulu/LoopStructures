b7e8ff44a248ee1228eb1f3acab54a2b194ac6b9
CChelseaLulu <90858702+CChelseaLulu@users.noreply.github.com>
2021-12-21 13:26:28 +0300

    1

---
+++ q1.py
@@ -0,0 +1,81 @@
+#Q1a
+sum = 0
+for i in range(0,101):
+    sum = sum + i
+print(sum)
+
+
+count = 0
+total = 0
+while (count < 100):
+    count = count + 1
+    total = total + count
+print(total)
+
+#Q1b
+sum = 0
+for i in range(5,101):
+    sum = sum + i
+print(sum)
+
+count = 0
+total = 0
+while (5 >= count and count <= 100):
+    count = count + 1
+    total = total + count
+print(total)
+
+
+first = int(input("Enter the start number:"))
+sum = 0
+for i in range(first,100 + 1):
+    sum = sum + i
+print(sum)
+
+
+first = int(input("Enter the start number:"))
+count = 0
+total = 0
+while (first < count < 100):
+    count = count + 1
+    total = total + count
+print(total)
+
+#q1c
+end= int(input("Enter the end number:"))
+sum = 0
+for i in range(0,end + 1):
+    sum = sum + i
+print(sum)
+
+
+end = int(input("Enter the end number:"))
+count = 0
+total = 0
+while (count < end):
+    count = count + 1
+    total = total + count
+print(total)
+
+
+#q1d
+end= int(input("Enter the end number:"))
+sum = 0
+for i in range(0,end + 2):
+    sum = sum + i
+print(sum)
+
+
+end = int(input("Enter the end number:"))
+count = 0
+total = 0
+while (count < end):
+    count = count + 2
+    total = total + count
+print(total)
+
+
+
+
+
+
