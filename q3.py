dba587107598d43abe4f2b286d6f14c7571a906e
CChelseaLulu <90858702+CChelseaLulu@users.noreply.github.com>
2021-12-21 16:15:58 +0300

    q3

---
+++ q3.py
@@ -0,0 +1,54 @@
+def finalval():
+    i = 25
+
+    while i > 12:
+        print(i)
+        i = i - 3
+    print("Final value:", i)
+
+finalval()
+
+#decrease by 2
+
+def finalval():
+    i = 25
+#q3a
+    while i > 12:
+        print(i)
+        i = i - 2
+    print("Final value:", i)
+
+finalval()
+
+#decrease by 4
+def finalval():
+    i = 25
+
+    while i > 12:
+        print(i)
+        i = i - 4
+    print("Final value:", i)
+
+finalval()
+
+#decrease by 5
+def finalval():
+    i = 25
+
+    while i > 12:
+        print(i)
+        i = i - 5
+    print("Final value:", i)
+
+finalval()
+
+#decrease by 10
+def finalval():
+    i = 25
+
+    while i > 12:
+        print(i)
+        i = i - 10
+    print("Final value:", i)
+
+finalval()
